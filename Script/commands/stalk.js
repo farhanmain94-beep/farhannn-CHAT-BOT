63dba5302998c0591402f6cebbafab980aee0e46
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 16:05:51 +0600

    Update stalk.js

--- Script/commands/stalk.js
+++ Script/commands/stalk.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "stalk",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "Shaon Ahmed",
+    credits: "𝐅𝐀𝐑𝐇𝐀𝐍 𝐈𝐒𝐋𝐀𝐌",
     description: "5 বারের জন্য ক্রমাগত বন্ধুর ট্যাগ ট্যাগ করুন\nসেই ব্যক্তিকে আত্মা কলিং বলা যেতে পারে",
     commandCategory: "nsfw",
     usages: " please @mention",
