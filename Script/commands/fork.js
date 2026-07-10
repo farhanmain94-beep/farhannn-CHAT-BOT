4d00e52e829bce3802b7c6a26fae73be2cf1abd6
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 14:10:11 +0600

    Update fork.js

--- Script/commands/fork.js
+++ Script/commands/fork.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "fork",
     version: "1.0.1",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
+    credits: "Farhan Islam",
     description: "Send YouTube channel and GitHub fork link with intro text",
     commandCategory: "other",
     usages: "fork",
@@ -15,7 +15,7 @@ module.exports.run = async function({ api, event }) {
         "আপনি যদি তৈরি করতে চান, তাহলে চলে যেতে পারেন আমাদের ইউটিউব চ্যানেলে। " +
         "সেখানে গিয়ে ভিডিও দেখে আপনি খুব সহজে বট তৈরি করে নিতে পারবেন। " +
         "তাহলে আর দেরি না করে এখনি চলে যান আমাদের ইউটিউব চ্যানেলে।\n\n" +
-        "➤ YouTube Channel: https://youtube.com/@scb-community\n\n" +
+        "➤ YouTube Channel: https://www.youtube.com/@Farhan-r6q5v\n\n" +
         "🔗 GitHub Fork Link: https://github.com/shahadat-sahu/SHAHADAT-CHAT-BOT";
 
     return api.sendMessage(message, event.threadID, event.messageID);
