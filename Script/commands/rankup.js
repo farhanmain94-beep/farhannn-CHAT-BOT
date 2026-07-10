52d0cdbbb224d54cb1d1874cc66b5d77d7ab1fff
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 15:53:35 +0600

    Update rankup.js

--- Script/commands/rankup.js
+++ Script/commands/rankup.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "rankup",
   version: "2.0.0",
   hasPermssion: 1,
-  credits: "SHAHADAT SAHU",
+  credits: "𝐅𝐀𝐑𝐇𝐀𝐍 𝐈𝐒𝐋𝐀𝐌",
   description: "Announce rankup for each group/user",
   commandCategory: "Edit-IMG",
   dependencies: { "fs-extra": "" },
