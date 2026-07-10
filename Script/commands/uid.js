fcd6167ec0ae1f55165768351c52d30dcc2bc17a
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 16:17:29 +0600

    Update uid.js

--- Script/commands/uid.js
+++ Script/commands/uid.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "uid",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "SHAHADAT SAHU",
+	credits: "𝐅𝐀𝐑𝐇𝐀𝐍 𝐈𝐒𝐋𝐀𝐌",
 	description: "Get UID (self, mention, or reply)",
 	commandCategory: "user",
 	usages: "uid [tag/reply/none]",
