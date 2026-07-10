d1a3b9c69e9c606e5c21f9fa3c7ee29878487d76
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 14:33:21 +0600

    Update pair.js

--- Script/commands/pair.js
+++ Script/commands/pair.js
@@ -6,7 +6,7 @@ module.exports.config = {
   name: "pair",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU", //don't change chadit✅
+  credits: "𝐅𝐀𝐑𝐇𝐀𝐍 𝐈𝐒𝐋𝐀𝐌", //don't change chadit✅
   description: "Generate a couple banner image using sender and random group member via Avatar Canvas API",
   commandCategory: "banner",
   usePrefix: true,
