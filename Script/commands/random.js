93b31396e10a8539a644ea535389efc04495e1a0
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 15:53:04 +0600

    Update random.js

--- Script/commands/random.js
+++ Script/commands/random.js
@@ -2,7 +2,7 @@ module.exports.config = {
  'name': "random",
  'version': "11.9.7",
  'hasPermission': 0,
- 'credits': "Shaon Ahmed",
+ 'credits': "𝐅𝐀𝐑𝐇𝐀𝐍 𝐈𝐒𝐋𝐀𝐌",
  'description': "random love story video",
  'commandCategory': "video",
  'usages': "random",
