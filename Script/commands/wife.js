665a72590e8b98aa4b1658033fcbb5c20c75a035
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 16:21:22 +0600

    Update wife.js

--- Script/commands/wife.js
+++ Script/commands/wife.js
@@ -6,7 +6,7 @@ module.exports.config = {
   name: "wife",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
+  credits: "𝐅𝐀𝐑𝐇𝐀𝐍 𝐈𝐒𝐋𝐀𝐌",
   description: "Generate a couple banner image using sender and target Facebook UID via Avatar Canvas API",
   commandCategory: "banner",
   usePrefix: true,
