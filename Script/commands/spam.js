49e8032b2a87b5126defd406d32cb56e95c0a7d9
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 16:05:02 +0600

    Update spam.js

--- Script/commands/spam.js
+++ Script/commands/spam.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "spam",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "SHAHADAT SAHU",
+  credits: "𝐅𝐀𝐑𝐇𝐀𝐍 𝐈𝐒𝐋𝐀𝐌",
   description: "",
   commandCategory: "spam",
   usages: "[msg] [amount]",
