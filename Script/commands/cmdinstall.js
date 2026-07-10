35350925e55e4960d42da4dec3962d0d4490ea76
farhanmain94-beep <farhanmain94@gmail.com>
2026-07-10 14:02:45 +0600

    Update cmdinstall.js

--- Script/commands/cmdinstall.js
+++ Script/commands/cmdinstall.js
@@ -7,7 +7,7 @@ module.exports.config = {
   name: "install",
   version: "2.0.0",
   hasPermssion: 2,
-  credits: "SHAHADAT SAHU",
+  credits: "Farhan Islam",
   description: "Create/Delete/Load modules",
   commandCategory: "System",
   usages: "[file.js code/link] / [del file.js]",
